540b7ff8262b340f4c8180e7089437dca374054b
sean.e <none@none>
2007-05-29 01:20:45 +0000

    add iface method for setting switch text

--- Engine/ISwitchDisplay.h
+++ Engine/ISwitchDisplay.h
@@ -1,6 +1,9 @@
 #ifndef ISwitchDisplay_h__
 #define ISwitchDisplay_h__
 
+#include <string>
+
+
 // ISwitchDisplay
 // ----------------------------------------------------------------------------
 // use to set / clear LEDs associated with the switches.
@@ -9,6 +12,7 @@ class ISwitchDisplay
 {
 public:
 	virtual void SetSwitchDisplay(int switchNumber, bool isOn) = 0;
+	virtual void SetSwitchText(const std::string & txt) = 0;
 };
 
 #endif // ISwitchDisplay_h__
