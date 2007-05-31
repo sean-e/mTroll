2c1bf85fd601ab81813114192c43fbde0f51731d
sean.e <none@none>
2007-05-31 07:22:35 +0000

    add stubs

--- mControlUI/mControlUIView.cpp
+++ mControlUI/mControlUIView.cpp
@@ -6,8 +6,99 @@
 #include "resource.h"
 
 #include "mControlUIView.h"
+#include "..\Engine\EngineLoader.h"
+#include "..\Engine\MidiControlEngine.h"
+
+
+CMControlUIView::CMControlUIView()
+{
+	EngineLoader ldr(this, this, this, this);
+	mEngine = ldr.CreateEngine("");
+}
+
+CMControlUIView::~CMControlUIView()
+{
+	delete mEngine;
+}
 
 BOOL CMControlUIView::PreTranslateMessage(MSG* pMsg)
 {
 	return CWindow::IsDialogMessage(pMsg);
+}
+
+
+// IMainDisplay
+void
+CMControlUIView::TextOut(const std::string & txt)
+{
+
+}
+
+void
+CMControlUIView::ClearDisplay()
+{
+
+}
+
+// ITraceDisplay
+void
+CMControlUIView::Trace(const std::string & txt)
+{
+
+}
+
+// ISwitchDisplay
+void
+CMControlUIView::SetSwitchDisplay(int switchNumber, bool isOn)
+{
+
+}
+
+bool
+CMControlUIView::SupportsSwitchText() const
+{
+	return true;
+}
+
+void
+CMControlUIView::SetSwitchText(int switchNumber, const std::string & txt)
+{
+
+}
+
+void
+CMControlUIView::ClearSwitchText(int switchNumber)
+{
+
+}
+
+// IMidiOut
+int
+CMControlUIView::GetDeviceCount()
+{
+	return 0;
+}
+
+std::string
+CMControlUIView::GetDeviceName(int deviceIdx)
+{
+	return "";
+}
+
+bool
+CMControlUIView::OpenMidiOut(int deviceIdx)
+{
+	return false;
+}
+
+bool
+CMControlUIView::MidiOut(const Bytes & bytes)
+{
+	return false;
+}
+
+void
+CMControlUIView::CloseMidiOut()
+{
+
 }
--- mControlUI/mControlUIView.h
+++ mControlUI/mControlUIView.h
@@ -4,12 +4,47 @@
 
 #pragma once
 
-class CMControlUIView : public CDialogImpl<CMControlUIView>
+#include "..\Engine\IMainDisplay.h"
+#include "..\Engine\IMidiOut.h"
+#include "..\Engine\ISwitchDisplay.h"
+#include "..\Engine\ITraceDisplay.h"
+
+class MidiControlEngine;
+
+
+class CMControlUIView : public CDialogImpl<CMControlUIView>, 
+								IMainDisplay, 
+								IMidiOut, 
+								ISwitchDisplay, 
+								ITraceDisplay
 {
 public:
+	CMControlUIView();
+	~CMControlUIView();
+
 	enum { IDD = IDD_MCONTROLUI_FORM };
 
 	BOOL PreTranslateMessage(MSG* pMsg);
+
+	// IMainDisplay
+	virtual void TextOut(const std::string & txt);
+	virtual void ClearDisplay();
+
+	// ITraceDisplay
+	virtual void Trace(const std::string & txt);
+
+	// ISwitchDisplay
+	virtual void SetSwitchDisplay(int switchNumber, bool isOn);
+	virtual bool SupportsSwitchText() const;
+	virtual void SetSwitchText(int switchNumber, const std::string & txt);
+	virtual void ClearSwitchText(int switchNumber);
+
+	// IMidiOut
+	virtual int GetDeviceCount();
+	virtual std::string GetDeviceName(int deviceIdx);
+	virtual bool OpenMidiOut(int deviceIdx);
+	virtual bool MidiOut(const Bytes & bytes);
+	virtual void CloseMidiOut();
 
 	BEGIN_MSG_MAP(CMControlUIView)
 	END_MSG_MAP()
@@ -18,4 +53,6 @@ public:
 //	LRESULT MessageHandler(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
 //	LRESULT CommandHandler(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
 //	LRESULT NotifyHandler(int /*idCtrl*/, LPNMHDR /*pnmh*/, BOOL& /*bHandled*/)
+private:
+	MidiControlEngine * mEngine;
 };
