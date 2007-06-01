da38a11473c09be9545960d5bd94e39bb8339c6c
sean.e <none@none>
2007-06-01 06:35:17 +0000

    added switch cntrls

--- mControlUI/resource.h
+++ mControlUI/resource.h
@@ -1,21 +1,64 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by mControlUI.RC
+// Used by mControlUI.rc
 //
-
-
-#define IDD_ABOUTBOX				100
-#define IDR_MAINFRAME				128
-//#define IDR_MCONTROLUITYPE	129
-#define IDD_MCONTROLUI_FORM	129
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDD_MCONTROLUI_FORM             129
+#define IDC_SWITCH1                     1000
+#define IDC_SWITCH_TEXT1                1001
+#define IDC_LED1                        1002
+#define IDC_BANKTEXT                    1003
+#define IDC_SWITCH2                     1006
+#define IDC_SWITCH_TEXT2                1007
+#define IDC_LED2                        1008
+#define IDC_SWITCH3                     1009
+#define IDC_SWITCH_TEXT3                1010
+#define IDC_LED3                        1011
+#define IDC_SWITCH4                     1012
+#define IDC_SWITCH_TEXT4                1013
+#define IDC_LED4                        1014
+#define IDC_SWITCH5                     1021
+#define IDC_SWITCH_TEXT5                1022
+#define IDC_LED5                        1023
+#define IDC_SWITCH6                     1024
+#define IDC_SWITCH_TEXT6                1025
+#define IDC_LED6                        1026
+#define IDC_SWITCH7                     1027
+#define IDC_SWITCH_TEXT7                1028
+#define IDC_LED7                        1029
+#define IDC_SWITCH8                     1030
+#define IDC_SWITCH_TEXT8                1031
+#define IDC_LED8                        1032
+#define IDC_SWITCH9                     1033
+#define IDC_SWITCH_TEXT9                1034
+#define IDC_LED9                        1035
+#define IDC_SWITCH10                    1036
+#define IDC_SWITCH_TEXT10               1037
+#define IDC_LED10                       1038
+#define IDC_SWITCH11                    1039
+#define IDC_SWITCH_TEXT11               1040
+#define IDC_LED11                       1041
+#define IDC_SWITCH12                    1042
+#define IDC_SWITCH_TEXT12               1043
+#define IDC_LED12                       1044
+#define IDC_SWITCH13                    1045
+#define IDC_SWITCH_TEXT13               1046
+#define IDC_LED13                       1047
+#define IDC_SWITCH14                    1048
+#define IDC_SWITCH_TEXT14               1049
+#define IDC_LED14                       1050
+#define IDC_SWITCH15                    1051
+#define IDC_SWITCH16                    1054
+#define IDC_LED16                       1056
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	201
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32772
+#define _APS_NEXT_RESOURCE_VALUE        201
+#define _APS_NEXT_COMMAND_VALUE         32772
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
