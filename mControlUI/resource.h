6e63ba57b2b08aa2d3f878143d8123139cd15c19
sean.e <none@none>
2007-09-17 01:33:36 +0000

    display build date in about dlg added new files to project

--- mControlUI/resource.h
+++ mControlUI/resource.h
@@ -5,6 +5,7 @@
 #define IDD_ABOUTBOX                    100
 #define IDR_MAINFRAME                   128
 #define IDD_MCONTROLUI_FORM             129
+#define IDC_BUILD_DATE                  1004
 
 // Next default values for new objects
 // 
@@ -12,7 +13,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        201
 #define _APS_NEXT_COMMAND_VALUE         32772
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
