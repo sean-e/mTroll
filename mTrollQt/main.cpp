a021240fa111cba2a7f22c9543b9a65c06f8df53
sean.e <none@none>
2008-05-31 20:47:19 +0000

    add exception translator

--- mTrollQt/main.cpp
+++ mTrollQt/main.cpp
@@ -1,9 +1,16 @@
 #include <QApplication>
 #include <qdebug.h>
 
+#if defined(_WINDOWS)
+#include "..\winUtil\SEHexception.h"
+#endif // _WINDOWS
+
 
 int main(int argc, char **argv)
 {
+#if defined(_WINDOWS)
+	::_set_se_translator(::trans_func);
+#endif // _WINDOWS
 	QApplication app(argc, argv);
 // 	QMap<QString, QSize> customSizeHints = parseCustomSizeHints(argc, argv);
 // 	MainWindow mainWin(customSizeHints);
@@ -13,3 +20,13 @@ int main(int argc, char **argv)
 	return 1;
 //	return app.exec();
 }
+
+
+#if defined(_WINDOWS)
+void 
+trans_func(unsigned int /*u*/, 
+		   EXCEPTION_POINTERS* /*pExp*/)
+{
+    throw SEHexception();
+}
+#endif // _WINDOWS
