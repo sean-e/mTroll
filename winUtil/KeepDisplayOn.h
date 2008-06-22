86e757b52dc796f6cd8748b9f0b03c6a697b426f
sean.e <none@none>
2008-06-22 23:40:00 +0000

    add include directive

--- winUtil/KeepDisplayOn.h
+++ winUtil/KeepDisplayOn.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <Windows.h>
+
 // KeepDisplayOn 
 // -----------------------------------------------------------------------------
 // This class should be used sparingly.  It violates the policies set in place
