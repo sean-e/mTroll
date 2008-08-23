15d755adf1930283acbf07c9ca8dd247ae6b138b
sean.e <none@none>
2008-08-23 21:40:21 +0000

    GPLv3

--- Engine/TwoStatePatch.h
+++ Engine/TwoStatePatch.h
@@ -1,3 +1,27 @@
+/*
+ * mTroll MIDI Controller
+ * Copyright (C) 2007-2008 Sean Echevarria
+ *
+ * This file is part of mTroll.
+ *
+ * mTroll is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mTroll is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Let me know if you modify, extend or use mTroll.
+ * Original project site: http://www.creepingfog.com/mTroll/
+ * Contact Sean: "fester" at the domain of the original project site
+ */
+
 #ifndef TwoStatePatch_h__
 #define TwoStatePatch_h__
 
--- mTrollQt/main.cpp
+++ mTrollQt/main.cpp
@@ -1,3 +1,27 @@
+/*
+ * mTroll MIDI Controller
+ * Copyright (C) 2007-2008 Sean Echevarria
+ *
+ * This file is part of mTroll.
+ *
+ * mTroll is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mTroll is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Let me know if you modify, extend or use mTroll.
+ * Original project site: http://www.creepingfog.com/mTroll/
+ * Contact Sean: "fester" at the domain of the original project site
+ */
+
 #include <QApplication>
 #include <qdebug.h>
 #include "MainTrollWindow.h"
@@ -17,13 +41,3 @@ int main(int argc, char **argv)
 	mainWin.show();
 	return app.exec();
 }
-
-
-#if defined(_WINDOWS)
-void 
-trans_func(unsigned int /*u*/, 
-		   EXCEPTION_POINTERS* /*pExp*/)
-{
-    throw SEHexception();
-}
-#endif // _WINDOWS
