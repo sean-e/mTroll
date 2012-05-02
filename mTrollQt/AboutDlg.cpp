667291dc473ea93fe8c211a14ffb2a8400925bd2
sean_e <none@none>
2012-05-01 23:09:54 -0700

    update copyright year

--- mTrollQt/AboutDlg.cpp
+++ mTrollQt/AboutDlg.cpp
@@ -1,6 +1,6 @@
 /*
  * mTroll MIDI Controller
- * Copyright (C) 2007-2011 Sean Echevarria
+ * Copyright (C) 2007-2012 Sean Echevarria
  *
  * This file is part of mTroll.
  *
@@ -40,7 +40,7 @@ AboutDlg::AboutDlg()
 		"<html><body>mTroll MIDI Controller<br><br>"
 		"<a href=\"http://www.creepingfog.com/mTroll/\">http://www.creepingfog.com/mTroll/</a><br><br>"
 		"<a href=\"http://sourceforge.net/projects/mtroll/\">http://sourceforge.net/projects/mtroll/</a><br><br>"
-		"&copy; copyright 2007-2011 Sean Echevarria<br><br>";
+		"&copy; copyright 2007-2012 Sean Echevarria<br><br>";
 	labelTxt += "Built " + ::GetBuildDate();
 	labelTxt += "</body></html>";
 
