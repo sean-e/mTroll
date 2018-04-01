80875181081f9644bffea52701e66968dd78e1fd
sean echevarria <gh@creepingfog.com>
2018-03-31 22:53:52 -0400

    release 2018.03.31 (unpublished)

--- mTrollQt/AboutDlg.cpp
+++ mTrollQt/AboutDlg.cpp
@@ -1,6 +1,6 @@
 /*
  * mTroll MIDI Controller
- * Copyright (C) 2007-2017 Sean Echevarria
+ * Copyright (C) 2007-2018 Sean Echevarria
  *
  * This file is part of mTroll.
  *
@@ -39,8 +39,9 @@ AboutDlg::AboutDlg()
 	QString labelTxt =
 		"<html><body>mTroll MIDI Controller<br><br>"
 		"<a href=\"http://www.creepingfog.com/mTroll/\">http://www.creepingfog.com/mTroll/</a><br><br>"
+		"<a href=\"https://github.com/sean-e/mtroll/\">https://github.com/sean-e/mtroll/</a><br><br>"
 		"<a href=\"http://sourceforge.net/projects/mtroll/\">http://sourceforge.net/projects/mtroll/</a><br><br>"
-		"&copy; copyright 2007-2017 Sean Echevarria<br><br>";
+		"&copy; copyright 2007-2018 Sean Echevarria<br><br>";
 	labelTxt += "Built " + ::GetBuildDate();
 	labelTxt += "</body></html>";
 
