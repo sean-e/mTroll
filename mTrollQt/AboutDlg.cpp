e30652a79dcc2b30547f877ebf63fb3bc6c7bebf
sean echevarria <gh@creepingfog.com>
2026-02-10 11:56:04 -0500

    update year

--- mTrollQt/AboutDlg.cpp
+++ mTrollQt/AboutDlg.cpp
@@ -1,6 +1,6 @@
 /*
  * mTroll MIDI Controller
- * Copyright (C) 2007-2018,2020,2021-2025 Sean Echevarria
+ * Copyright (C) 2007-2018,2020,2021-2026 Sean Echevarria
  *
  * This file is part of mTroll.
  *
@@ -40,7 +40,7 @@ AboutDlg::AboutDlg(QWidget * parent) : QDialog(parent)
 		"<html><body>mTroll MIDI Controller<br><br>"
 		"<a href=\"http://www.creepingfog.com/mTroll/\">http://www.creepingfog.com/mTroll/</a><br><br>"
 		"<a href=\"https://github.com/sean-e/mtroll/\">https://github.com/sean-e/mtroll/</a><br><br>"
-		"&copy; copyright 2007-2025 Sean Echevarria<br><br>"
+		"&copy; copyright 2007-2026 Sean Echevarria<br><br>"
 		"MIDI beat clock thread &copy; copyright 2016 Pete Brown<br><br>";
 	labelTxt += "Built " + ::GetBuildDate() + 
 #ifdef _M_X64
